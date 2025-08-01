b77d370ba97c1d7086cdd226c969a5749793ef8b
Eas <me@eas.web.id>
2025-08-01 22:39:15 +0700

    index: add static path, `POST /chat` endpoint

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import express from 'express';
 import multer from 'multer';
 import fs from 'fs/promises';
 import { GoogleGenAI } from '@google/genai';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import cors from 'cors';
 
 const extractGeneratedText = (data) => {
     try {
@@ -23,18 +26,41 @@ const app = express();
 // declare variable untuk multer
 const upload = multer();
 
+// buat 2 variable ajaib (magic variable)
+// __
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // declare variable default-nya
 const DEFAULT_GEMINI_MODEL = 'gemini-2.5-flash';
 const DEFAULT_PORT = 3000;
+
+// util variable untuk membuat satu object mapper
+const modelMapper = {
+    'flash': 'gemini-2.5-flash',
+    'flash-lite': 'gemini-2.5-flash-lite',
+    'pro': 'gemini-2.5-pro'
+}
+
+// helper function yang menentukan model mana yang digunakan
+const determineGeminiModel = (key) => {
+    return modelMapper[key] ?? DEFAULT_GEMINI_MODEL;
+}
 
 // instantiation --> memanggil class menjadi sebuah instance
 const ai = new GoogleGenAI({
     apiKey: process.env.GOOGLE_AI_STUDIO_API_KEY
 });
 
+// memanggil middleware untuk bisa handle CORS
+app.use(cors());
+
 // memanggil middleware untuk bisa terima header
 // dengan Content-Type: application/json
 app.use(express.json());
+
+// panggil middleware untuk serve static file
+app.use(express.static(path.join(__dirname, 'public')));
 
 // tambah routing untuk handle model-nya
 // app.post('/', (req, res) => {})
@@ -59,6 +85,52 @@ app.post('/generate-text', async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 });
+
+app.post('/chat', async (req, res) => {
+    try {
+        if (!req.body) {
+            // perlu ada payload yang dikirim
+            // HTTP 400 --> Bad Request
+            //      ^   --> user yang melakukan "kesalahan"
+            //       ^^ --> 404 --> Not Found (nggak ketemu halaman/item-nya)
+            //          --> 403 --> Forbidden (akses tidak dibolehkan)
+            //          --> 401 --> Unauthorized (belum login)
+            return res.json(400, "Invalid request body!");
+        }
+
+        // extract messages dari request body
+        const { messages } = req.body;
+
+        // cek messages-nya
+        if (!messages) {
+            // kirim kalau nggak ada message-nya
+            return res.json(400, "Pesannya masih kosong nih!");
+        }
+
+        const payload = messages.map(
+            msg => {
+                return {
+                    role: msg.role,
+                    parts: [
+                        { text: msg.content }
+                    ]
+                }
+            }
+        );
+
+        const aiResponse = await ai.models.generateContent({
+            model: determineGeminiModel('pro'),
+            contents: payload,
+            config: {
+                systemInstruction: "Anda adalah chatter terhandal."
+            }
+        });
+
+        res.json({ reply: extractGeneratedText(aiResponse) });
+    } catch (e) {
+        res.status(500).json({ message: e.message });
+    }
+})
 
 app.post(
     '/generate-text-from-image',
